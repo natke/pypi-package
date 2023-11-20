2cd53ad63f142a62b9af82069ddfbe1f3fe279fb
Nat Kershaw <nakersha@microsoft.com>
2023-11-20 14:35:05 -0800

    Add placeholder Python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='onnxruntime-genai',
+      version='0.0.1',
+      description='Placeholder',
+      url='http://github.com/natke/pypi-package',
+      author='natke',
+      author_email='nakersha@microsoft.com',
+      license='MIT',
+      packages=['onnxruntime_genai'],
+      zip_safe=False)
